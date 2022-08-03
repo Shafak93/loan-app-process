1b956618df2ee86036cd15886d0c62c6c2d4f4de
Shafak93 <shafakctg64@gmail.com>
2022-08-03 12:07:13 +0600

    Project setup and component create

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import loan from '../../images/loan.jpg'
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-5xl font-bold text-white text-center mt-48'>Get your loan approved in 3 steps </h1>
+            <button> Get Started</button>
+        </div>
+    );
+};
+
+export default Home;
